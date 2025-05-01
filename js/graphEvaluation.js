3296c126cd819fe3db3abdb3531d3347d63b6de4
kaakn... <kaan.d.kahvecioglu@icloud.com>
2025-05-01 12:53:00 -0400

    Update graphEvaluation.js

--- js/graphEvaluation.js
+++ js/graphEvaluation.js
@@ -8,6 +8,9 @@ $(document).ready(function () {
         console.log(Element.allElements);
 
         evaluateGraph(Element.allElements, currentGraph.criteria, ctx);
+        $('html,body').animate({
+            scrollTop: $("#evaluations-container").offset().top},
+            'slow');
     });
 });
 
